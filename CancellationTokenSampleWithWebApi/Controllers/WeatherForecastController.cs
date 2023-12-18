9d7030d520fabe671fa19e3f2bb07373fb561818
ishfaq babar <ishfaq_ciit@yahoo.com>
2023-12-18 15:41:59 +0500

    Update Documentations

--- CancellationTokenSampleWithWebApi/Controllers/WeatherForecastController.cs
+++ CancellationTokenSampleWithWebApi/Controllers/WeatherForecastController.cs
@@ -14,6 +14,7 @@ public class WeatherForecastController : ControllerBase
     public WeatherForecastController(ILogger<WeatherForecastController> logger,
         ApplicationDbContext context)
     {
+        // Constructor to initialize logger and database context.
         _logger = logger;
         _context = context;
     }
@@ -22,6 +23,8 @@ public class WeatherForecastController : ControllerBase
     [HttpGet("no-cancellation")]
     public async Task<ActionResult> Get()
     {
+        // API endpoint with no cancellation token.
+        // Performs a simple task that takes 30 seconds.
         await DoSomething(Random.Shared.Next(1, 1000));
 
         return Ok("DONE!!!");
@@ -31,16 +34,29 @@ public class WeatherForecastController : ControllerBase
     [HttpGet("with-cancellation-source")]
     public async Task<ActionResult> GetWithCancellationSource()
     {
+        // API endpoint with a cancellation token source.
+        // Automatically cancels the task if it runs for more than 10 seconds.
+
         using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
 
         await DoSomething(Random.Shared.Next(1, 1000), cancellationTokenSource.Token);
         return Ok("DONE!!!");
     }
 
-
+    /// <summary>
+    /// This will abort the current request in a long running process from client side , browser refresh, browser close ..  and also will close the all depending processes
+    /// long running dependence processes Api=> services => Repository code etc
+    /// But if a process is running in detached mode that will not be cancel,  for example if current request run a long running query on SQL server even though request is canceled but query on SQL will be not aborted.
+    /// here HttpContext.RequestAborted is cancellation token already built into HttpContext so cancellationTokenSource is not needed in method parameter.
+    /// </summary>
+    /// <returns></returns>
     [HttpGet("with-request-cancellation")]
     public async Task<ActionResult> GetWithCancellationRequest()
     {
+
+        // API endpoint using the built-in cancellation token from HttpContext.
+        // Cancels the task if the client closes the browser or refreshes the page.
+
         await DoSomething(Random.Shared.Next(1, 1000), HttpContext.RequestAborted);
 
         /*
@@ -56,16 +72,22 @@ public class WeatherForecastController : ControllerBase
     [HttpGet("with-ef-query-cancellation")]
     public async Task<ActionResult> GetWithCancellationQuert()
     {
+
+        //waitfor delay '00:00:10' is used in SQL to instruct SQL server to run a query after delay .
+        // API endpoint demonstrating cancellation of a long-running Entity Framework query.
         await _context.Database
-            .ExecuteSqlRawAsync("waitfor delay '00:00:10'; select * from Test", HttpContext.RequestAborted);
+            .ExecuteSqlRawAsync("waitfor delay '00:00:10'; select * from Test", HttpContext.RequestAborted); 
 
         return Ok("DONE!!!");
     }
 
+    //Mock Up for Console output
     private async Task DoSomething(int number = 0, CancellationToken cancellationToken = default)
     {
         try
         {
+            // Simulates a task that runs for 30 seconds, writing to the console.
+
             for (int i = 0; i < 30; i++)
             {
                 await Task.Delay(1000, cancellationToken);
@@ -74,6 +96,7 @@ public class WeatherForecastController : ControllerBase
         }
         catch (Exception ex)
         {
+            // Logs any errors that may occur during the task.
             _logger.LogError(ex.Message);
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # How to use CancellationToken in ASP.NET WebApi
 
+#Cancellation Token Web API Sample
+##Overview
+This repository explores the usage of cancellation tokens in a web API, focusing on scenarios where tasks need to be canceled for effective resource management. The provided ASP.NET project demonstrates various API endpoints employing different methods of implementing cancellation tokens.
+
+##Key Points
+1. No Cancellation Token
+- Simple API endpoint without cancellation token.
+- Demonstrates a basic task that takes 30 seconds.
+2. Cancellation Token Source
+- API endpoint using **`'CancellationTokenSource'`**.
+- Automatically cancels tasks running for more than 10 seconds.
+3. Request Cancellation
+- API endpoint utilizing built-in cancellation token from **`'HttpContext'`**.
+- Cancels tasks if the client closes the browser or refreshes the page.
+4. EF Query Cancellation
+- API endpoint illustrating cancellation of long-running Entity Framework query.
+5. Insights and Tips
+- Emphasizes the importance of cancellation tokens in releasing resources.
+- Use SQL Server Profiler tool for tracing database queries.
+##Conclusion
+This repository concludes by stressing the significance of cancellation tokens in managing asynchronous tasks, releasing resources, and ensuring efficient application performance. It provides practical insights for developers seeking to implement cancellation tokens in web APIs.
